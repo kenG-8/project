2d351f9ab1ba63898d33932c71d994b78940e3c8
kenG-8 <kengtop24@gmail.com>
2025-11-30 23:02:35 +0700

    Delete learnhtml directory

--- learnhtml/t.js
+++
@@ -1,9 +0,0 @@
-const flower=[
-    "🌹", "🌺", "🌻", "🌼", "💐", "🏵️"
-];
-function createflowr(){
-    const flowerdiv=document.createElement("div");
-    flowerdiv.className="flower";
-    const flowericon=flower[Math.floor(Math.random()*flower.length)];
-    const index=Math.random()*window*wv
-}
